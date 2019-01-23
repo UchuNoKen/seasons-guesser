e3c07510cf84d707ccbe9f9d6679ffd0e893dc85
UchuNoKen <xen.o.fite@gmail.com>
2019-01-23 14:02:24 -0500

    Refactored data loading to componentDidMount method

--- src/index.js
+++ src/index.js
@@ -5,19 +5,13 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errorMessage: "" };
-
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
   }
 
   componentDidMount() {
-    console.log("Component was rendered!");
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
   }
 
   componentDidUpdate() {
