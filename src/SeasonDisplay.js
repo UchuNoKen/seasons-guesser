11fc14bb5ae6d9f2ed3b3b246d1f0bd0e396ae04
UchuNoKen <xen.o.fite@gmail.com>
2019-01-23 15:54:59 -0500

    Added further styling for icons and background.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
 const seasonConfig = {
@@ -24,7 +25,7 @@ const SeasonDisplay = props => {
   const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
+    <div className={`season-display ${season}`}>
       <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
       <i className={`icon-right massive ${iconName} icon`} />
