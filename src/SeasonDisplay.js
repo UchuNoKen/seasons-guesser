59edc75436cacb19d06932325b066250d73df1b1
UchuNoKen <xen.o.fite@gmail.com>
2019-01-23 15:23:55 -0500

    Updated SeasonDisplay to show an icon.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,11 +10,14 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === "winter" ? "Burr, it's chilly!" : "Let's hit the beach!";
+  const text = season === "winter" ? `Burr, it's chilly!` : `Let's hit the beach!`;
+  const icon = season === "winter" ? "snowflake" : "sun";
 
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
