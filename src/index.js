da2a57a36a74faf4120bbfe52ba45689f7bf0642
UchuNoKen <xen.o.fite@gmail.com>
2019-01-23 13:21:47 -0500

    Added state and conditional rendering to App component.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props){
+    this.state = { lat: null, errorMessage: '' };
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
+      position => {
+        this.setState({ lat: position.coords.latitude })
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
-    return <div>Latitude: </div>;
+  }
+
+  render() {
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: { this.state.errorMessage }</div>
+    }
+
+    if(!this.state.errorMessage && this.state.lat){
+      return <div>Latitude: { this.state.lat }</div>
+    }
+
+    return <div>Loading...</div>
   }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+return (
+  <div>
+  
+  <br />
+  
+);
