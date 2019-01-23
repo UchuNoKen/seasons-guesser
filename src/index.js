39473b99086bfb71796793d449038dfe628d0901
UchuNoKen <xen.o.fite@gmail.com>
2019-01-22 20:48:05 -0500

    More scaffolding, created SeasonDisplay component.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Sample Text</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
