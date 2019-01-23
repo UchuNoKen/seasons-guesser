b2636c161d208b9ee242491167b27b937db97066
UchuNoKen <xen.o.fite@gmail.com>
2019-01-23 16:19:42 -0500

    Added default prop for loader message.

--- src/Loader.js
+++ src/Loader.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const Loader = () => {
+const Loader = props => {
   return (
     <div className="ui active dimmer">
-      <div className="ui big text loader">Loading...</div>
+      <div className="ui big text loader">{props.message}</div>
     </div>
   );
+};
+
+Loader.defaultProps = {
+  message: "Loading..."
 };
 
 export default Loader;
