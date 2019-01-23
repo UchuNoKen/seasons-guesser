e423976d0ba2a518fe9cc1bf36147ff126de331c
UchuNoKen <xen.o.fite@gmail.com>
2019-01-23 15:17:31 -0500

    Added logic to display text content.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,10 +10,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === "winter" ? "Burr, it's chilly!" : "Let's hit the beach!";
 
-  console.log(season);
-
-  return <div>Season Display</div>;
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
