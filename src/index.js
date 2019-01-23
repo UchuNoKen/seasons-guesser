3b08520615c08d0289532c4d44d5cf293075a922
UchuNoKen <xen.o.fite@gmail.com>
2019-01-23 16:11:09 -0500

    Added logic for Loader, added Loader component to App.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -25,7 +26,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading...</div>;
+    return <Loader />;
   }
 }
 
